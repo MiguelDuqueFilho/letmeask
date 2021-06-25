03a4998ae58dae5a77d3b6a393a351de44696c3e
Miguel Duque <miguel.duque@globo.com>
2021-06-24 22:12:03 -0300

    aula 5

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -10,7 +10,7 @@ type FirebaseQuestions = Record<
       avatar: string;
     };
     content: string;
-    isHighlighted: boolean;
+    isHighLighted: boolean;
     isAnswered: boolean;
     likes: Record<string, { authorId: string }>;
   }
@@ -23,7 +23,7 @@ type QuestionType = {
     avatar: string;
   };
   content: string;
-  isHighlighted: boolean;
+  isHighLighted: boolean;
   isAnswered: boolean;
   likeCount: number;
   likeId: string | undefined;
@@ -48,7 +48,7 @@ export function useRoom(roomId: string) {
             id: key,
             content: value.content,
             author: value.author,
-            isHighlighted: value.isHighlighted,
+            isHighLighted: value.isHighLighted,
             isAnswered: value.isAnswered,
             likeCount: Object.values(value.likes ?? {}).length,
             likeId: Object.entries(value.likes ?? {}).find(
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -4,6 +4,9 @@ import '../styles/room.scss';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
+
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import { Question } from '../components/Question';
@@ -27,6 +30,18 @@ export function AdminRoom() {
       endedAt: new Date(),
     });
     history.push('/');
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighLighted: true,
+    });
   }
 
   async function handleDeleteQuestion(questionId: string) {
@@ -62,7 +77,28 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighLighted={question.isHighLighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type='button'
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img
+                        src={checkImg}
+                        alt='Marcar pergunta como Respondida'
+                      />
+                    </button>
+                    <button
+                      type='button'
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt='Dar destaque à pergunta' />
+                    </button>
+                  </>
+                )}
                 <button
                   type='button'
                   onClick={() => handleDeleteQuestion(question.id)}
